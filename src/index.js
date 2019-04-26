f33b3eff8f156f9eef5416427a4de81467743d1d
Shaun Donnelly <shaundonnelly@gmail.com>
2019-04-26 08:57:22 +0100

    Initial commit from Create React App

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'bpk-stylesheets/base';
+import 'bpk-stylesheets/base.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
